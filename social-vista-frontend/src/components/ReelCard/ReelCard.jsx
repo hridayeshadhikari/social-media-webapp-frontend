da79b043bbafe3ad5ed7106751b3ece8f5dcf2b4
Hridayesh Adhikari <harshitadhikari99@gmail.com>
2024-01-23 15:17:26 +0530

    some mote thing added to user profile section such reel card etc.

---
+++ social-vista-frontend/src/components/ReelCard/ReelCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ReelCard = () => {
+  return (
+    <div className='w-[18rem] px-2 my-5'>
+      <video controls className='w-full h-full' src='https://player.vimeo.com/external/420239207.sd.mp4?s=2b5a6633c37af1a6fb0beb02c06bdc376fdfcce2&profile_id=165&oauth2_token_id=57447761
+      '/>
+    </div>
+  )
+}
+
+export default ReelCard
--- social-vista-frontend/src/pages/profile/Profile.jsx
+++ social-vista-frontend/src/pages/profile/Profile.jsx
@@ -6,6 +6,7 @@ import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Box from '@mui/material/Box';
 import PostCard from '../../components/PostCard/PostCard'
+import ReelCard from '../../components/ReelCard/ReelCard';
 
 const tabs = [{ value: "post", name: "Posts" },
 { value: "reels", name: "Reels" },
@@ -13,6 +14,9 @@ const tabs = [{ value: "post", name: "Posts" },
 { value: "repost", name: "Repost" },
 ]
 const posts = [1, 1, 1, 1, 1];
+const reels = [1, 1, 1, 1, 1, 1];
+const savePost = [1, 1, 1, 1, 1];
+
 const Profile = () => {
   const [value, setValue] = React.useState('post');
 
@@ -58,11 +62,19 @@ const Profile = () => {
           </Box>
           <div className='flex justify-center'>
             {value === "post" ? (<div className='space-y-5 w-[70%] my-10'>
-              {posts.map((item) => (<div className='border border-slate-400 rounded-md'>
+              {posts.map((item) => (<div className='border border-slate-100 rounded-md'>
                 <PostCard />
               </div>
               ))}
-            </div>) : ""}
+            </div>) : value === "reels" ? <div className='flex gap-2 flex-wrap justify-center'>
+              {reels.map((item) => <ReelCard />)}
+            </div> : value === "saved" ? <div className='space-y-5 w-[70%] my-10'>
+              {savePost.map((item) => (<div className='border border-slate-100 rounded-md'>
+                <PostCard />
+              </div>))}
+            </div> : (
+              <div>Repost</div>
+            )}
           </div>
         </section>
       </div>
