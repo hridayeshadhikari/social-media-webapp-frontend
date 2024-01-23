9650e45e0a33676065453301302a43c62d9cc6f4
Hridayesh Adhikari <harshitadhikari99@gmail.com>
2024-01-23 13:27:28 +0530

    profile page almost completed and implemented posts section in profile page

--- social-vista-frontend/src/pages/Home/Home.jsx
+++ social-vista-frontend/src/pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import {Grid } from '@mui/material';
+import { Grid } from '@mui/material';
 import React from 'react';
 import Sidebar from '../../components/sidebar/Sidebar';
 import { Route, Routes, useLocation } from 'react-router-dom';
@@ -9,38 +9,40 @@ import Profile from '../profile/Profile';
 import HomeRight from '../../components/homeright/HomeRight';
 
 const Home = () => {
-  const location=useLocation();
+  const location = useLocation();
   return (
-   
-      <div className='20px'> 
-        <Grid container spacing={0} >
-          <Grid item xs={0} lg={3}>
-            <div className='sticky top-0'>
-              <Sidebar />
-            </div>
-          </Grid>
 
-          <Grid lg={location.pathname==="/"?6:9} item className='px-5 flex justify-center' xs={12}>
-            
-              <Routes>
-                <Route path='/' element={<MiddlePart />}></Route>
-                <Route path='/reels' element={<Reels />}></Route>
-                <Route path='/create-reel' element={<CreateReel />}></Route>
-                <Route path='/profile/:id' element={<Profile />}></Route>
-              </Routes>
-          
-          </Grid>
-
-          <Grid item lg={3} className='relative'>
-           
-              <div className='sticky top-0 w-full '>
-                <HomeRight />
-              </div>
-          
-          </Grid>
+    <div className='20px'>
+      <Grid container spacing={0} >
+        <Grid item xs={0} lg={3}>
+          <div className='sticky top-0'>
+            <Sidebar />
+          </div>
         </Grid>
-      </div>
-   
+
+        <Grid lg={location.pathname === "/" ? 6 : 9} item className='px-5 flex justify-center' xs={12}>
+
+          <Routes>
+            <Route path='/' element={<MiddlePart />}></Route>
+            <Route path='/reels' element={<Reels />}></Route>
+            <Route path='/create-reel' element={<CreateReel />}></Route>
+            <Route path='/profile/:id' element={<Profile />}></Route>
+          </Routes>
+
+        </Grid>
+
+        {location.pathname === "/" &&
+          <Grid item lg={3} className='relative'>
+
+            <div className='sticky top-0 w-full '>
+              <HomeRight />
+            </div>
+
+          </Grid>
+        }
+      </Grid>
+    </div>
+
   );
 }
 
--- social-vista-frontend/src/pages/profile/Profile.jsx
+++ social-vista-frontend/src/pages/profile/Profile.jsx
@@ -1,12 +1,72 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
+import Avatar from '@mui/material/Avatar'
+import { Button, Card } from '@mui/material';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Box from '@mui/material/Box';
+import PostCard from '../../components/PostCard/PostCard'
 
+const tabs = [{ value: "post", name: "Posts" },
+{ value: "reels", name: "Reels" },
+{ value: "saved", name: "Saved" },
+{ value: "repost", name: "Repost" },
+]
+const posts = [1, 1, 1, 1, 1];
 const Profile = () => {
-    const {id}=useParams();
+  const [value, setValue] = React.useState('post');
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+  const { id } = useParams();
   return (
-    <div>
-      <h1>profile {id}</h1>
-    </div>
+    <Card className='my-10 w-[70%]'>
+      <div className='rounded-md'>
+        <div className='h-[14rem]'>
+          <img className='w-full h-full rounded-t-md' src="https://images.pexels.com/photos/414612/pexels-photo-414612.jpeg?cs=srgb&dl=pexels-james-wheeler-414612.jpg&fm=jpg" alt="" />
+        </div>
+        <div className='px-5 flex justify-between items-start mt-5 h-[5rem]'>
+          <Avatar className='tarnsform -translate-y-24' sx={{ height: "8rem", width: "8rem" }} src='https://img.freepik.com/free-photo/androgynous-avatar-non-binary-queer-person_23-2151100226.jpg?t=st=1705995186~exp=1705998786~hmac=9a0d68b55d7d18fb8ac72fdf8d1875d797b0bb7ebe28b1ed8fd8ff6c1aa486fc&w=740' />
+          {true ? <Button sx={{ borderRadius: "20px" }} variant='outlined'>Edit Profile</Button>
+            : <Button sx={{ borderRadius: "20px" }} variant='outlined'>Follow</Button>}
+        </div>
+        <div className='p-5'>
+          <div>
+            <h1 className='py-1 font-bold text-xl'>Hridayesh Adhikari</h1>
+            <p>@Harry</p>
+          </div>
+          <div className='flex gap-3 items-center py-3'>
+            <span>41 post</span>
+            <span>35 follower</span>
+            <span>40 followings</span>
+          </div>
+          <div>
+            <p>my name is hridayesh adhikari</p>
+          </div>
+
+        </div>
+        <section>
+          <Box sx={{ width: '100%', borderBottom: 1, borderColor: "divider" }}>
+            <Tabs
+              value={value}
+              onChange={handleChange}
+              aria-label="wrapped label tabs example"
+            >
+              {tabs.map((item) => (<Tab value={item.value} label={item.name} wrapped />))}
+            </Tabs>
+          </Box>
+          <div className='flex justify-center'>
+            {value === "post" ? (<div className='space-y-5 w-[70%] my-10'>
+              {posts.map((item) => (<div className='border border-slate-400 rounded-md'>
+                <PostCard />
+              </div>
+              ))}
+            </div>) : ""}
+          </div>
+        </section>
+      </div>
+    </Card>
   )
 }
 
