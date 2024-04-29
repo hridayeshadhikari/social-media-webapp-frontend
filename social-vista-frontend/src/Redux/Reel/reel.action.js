06718ae8f44c84917e30ec7128d274a74bec3c06
Hridayesh Adhikari <harshitadhikari99@gmail.com>
2024-04-29 22:46:30 +0530

    create reel action

--- social-vista-frontend/src/Redux/Reel/reel.action.js
+++ social-vista-frontend/src/Redux/Reel/reel.action.js
@@ -1,5 +1,5 @@
 import { api } from "../../config/api"
-import { GET_ALL_REELS_FAILURE, GET_ALL_REELS_REQUEST, GET_ALL_REELS_SUCCESS } from "./reel.actionType"
+import { CREATE_REEL_FAILURE, CREATE_REEL_REQUEST, CREATE_REEL_SUCCESS, GET_ALL_REELS_FAILURE, GET_ALL_REELS_REQUEST, GET_ALL_REELS_SUCCESS } from "./reel.actionType"
 
 export const getAllReels=()=>async (dispatch)=>{
     dispatch({type:GET_ALL_REELS_REQUEST})
@@ -9,5 +9,15 @@ export const getAllReels=()=>async (dispatch)=>{
         // console.log("<========>",data)
     } catch (error) {
         dispatch({type:GET_ALL_REELS_FAILURE,payload:error})
+    }
+}
+
+export const createReel=(reelData)=>async (dispatch)=>{
+    dispatch({type:CREATE_REEL_REQUEST})
+    try {
+        const {data}=await api.post("/api/reel/create",reelData)
+        dispatch({type:CREATE_REEL_SUCCESS,payload:data})
+    } catch (error) {
+        dispatch({type:CREATE_REEL_FAILURE,payload:error})
     }
 }
